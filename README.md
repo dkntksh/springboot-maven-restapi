39ae90ac671f366cef2486978c1efb6b480175e8
dnkntksh <mr.kaz71@gmail.com>
2021-05-25 13:43:22 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Greet Test
+RESTFUL API
+
+http://localhost:8080/greeting
+→{"id":1,"content":"Hello, World!"}
+
+http://localhost:8080/greeting?name=User
+→{"id":1,"content":"Hello, User!"}
---
+++ spgngbt-api/src/test/java/com/app/spgngbtapi/SpgngbtApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.spgngbtapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpgngbtApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
